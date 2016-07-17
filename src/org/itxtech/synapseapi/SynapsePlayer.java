c9419b5a09f75480be08e1d9b7c252ddd523cdb4
boybook <fangyizhou1997@qq.com>
2016-07-17 11:10:20 +0800

    Slow Login!

--- src/org/itxtech/synapseapi/SynapsePlayer.java
+++ src/org/itxtech/synapseapi/SynapsePlayer.java
@@ -35,6 +35,8 @@ public class SynapsePlayer extends Player {
     private long lastPacketTime;
     public boolean isSynapseLogin = false;
 
+    private long needSlowLogin = 0;
+
     public SynapsePlayer(SourceInterface interfaz, Long clientID, String ip, int port) {
         super(interfaz, clientID, ip, port);
     }
@@ -60,157 +62,169 @@ public class SynapsePlayer extends Player {
             super.processLogin();
             return;
         }
-        if (!this.server.isWhitelisted((this.getName()).toLowerCase())) {
-            this.close(this.getLeaveMessage(), "Server is white-listed");
+        if (needSlowLogin == 0) {
+            if (!this.server.isWhitelisted((this.getName()).toLowerCase())) {
+                this.close(this.getLeaveMessage(), "Server is white-listed");
 
-            return;
-        } else if (this.server.getNameBans().isBanned(this.getName().toLowerCase()) || this.server.getIPBans().isBanned(this.getAddress())) {
-            this.close(this.getLeaveMessage(), "You are banned");
+                return;
+            } else if (this.server.getNameBans().isBanned(this.getName().toLowerCase()) || this.server.getIPBans().isBanned(this.getAddress())) {
+                this.close(this.getLeaveMessage(), "You are banned");
 
-            return;
-        }
+                return;
+            }
 
-        if (this.hasPermission(Server.BROADCAST_CHANNEL_USERS)) {
-            this.server.getPluginManager().subscribeToPermission(Server.BROADCAST_CHANNEL_USERS, this);
-        }
-        if (this.hasPermission(Server.BROADCAST_CHANNEL_ADMINISTRATIVE)) {
-            this.server.getPluginManager().subscribeToPermission(Server.BROADCAST_CHANNEL_ADMINISTRATIVE, this);
-        }
+            if (this.hasPermission(Server.BROADCAST_CHANNEL_USERS)) {
+                this.server.getPluginManager().subscribeToPermission(Server.BROADCAST_CHANNEL_USERS, this);
+            }
+            if (this.hasPermission(Server.BROADCAST_CHANNEL_ADMINISTRATIVE)) {
+                this.server.getPluginManager().subscribeToPermission(Server.BROADCAST_CHANNEL_ADMINISTRATIVE, this);
+            }
 
-        for (Player p : new ArrayList<>(this.server.getOnlinePlayers().values())) {
-            if (p != this && p.getName() != null && this.getName() != null && Objects.equals(p.getName().toLowerCase(), this.getName().toLowerCase())) {
-                if (!p.kick("logged in from another location")) {
-                    this.close(this.getLeaveMessage(), "Logged in from another location");
+            for (Player p : new ArrayList<>(this.server.getOnlinePlayers().values())) {
+                if (p != this && p.getName() != null && this.getName() != null && Objects.equals(p.getName().toLowerCase(), this.getName().toLowerCase())) {
+                    if (!p.kick("logged in from another location")) {
+                        this.close(this.getLeaveMessage(), "Logged in from another location");
 
-                    return;
+                        return;
+                    }
+                } else if (p.loggedIn && this.getUniqueId().equals(p.getUniqueId())) {
+                    if (!p.kick("logged in from another location")) {
+                        this.close(this.getLeaveMessage(), "Logged in from another location");
+
+                        return;
+                    }
                 }
-            } else if (p.loggedIn && this.getUniqueId().equals(p.getUniqueId())) {
-                if (!p.kick("logged in from another location")) {
-                    this.close(this.getLeaveMessage(), "Logged in from another location");
+            }
 
+            this.setNameTag(this.username);
+
+            CompoundTag nbt = this.server.getOfflinePlayerData(this.username);
+            if (nbt == null) {
+                this.close(this.getLeaveMessage(), "Invalid data");
+
+                return;
+            }
+
+            this.playedBefore = (nbt.getLong("lastPlayed") - nbt.getLong("firstPlayed")) > 1;
+
+            nbt.putString("NameTag", this.username);
+
+            int exp = nbt.getInt("EXP");
+            int expLevel = nbt.getInt("expLevel");
+            this.setExperience(exp, expLevel);
+
+            this.gamemode = nbt.getInt("playerGameType") & 0x03;
+            if (this.server.getForceGamemode()) {
+                this.gamemode = this.server.getGamemode();
+                nbt.putInt("playerGameType", this.gamemode);
+            }
+
+            this.allowFlight = this.isCreative();
+
+            Level level;
+            if ((level = this.server.getLevelByName(nbt.getString("Level"))) == null) {
+                this.setLevel(this.server.getDefaultLevel());
+                nbt.putString("Level", this.level.getName());
+                nbt.getList("Pos", DoubleTag.class)
+                        .add(new DoubleTag("0", this.level.getSpawnLocation().x))
+                        .add(new DoubleTag("1", this.level.getSpawnLocation().y))
+                        .add(new DoubleTag("2", this.level.getSpawnLocation().z));
+            } else {
+                this.setLevel(level);
+            }
+
+            //todo achievement
+            nbt.putLong("lastPlayed", System.currentTimeMillis() / 1000);
+
+            if (this.server.getAutoSave()) {
+                this.server.saveOfflinePlayerData(this.username, nbt, true);
+            }
+
+            ListTag<DoubleTag> posList = nbt.getList("Pos", DoubleTag.class);
+
+            super.init(this.level.getChunk((int) posList.get(0).data >> 4, (int) posList.get(2).data >> 4, true), nbt);
+
+            if (!this.namedTag.contains("foodLevel")) {
+                this.namedTag.putInt("foodLevel", 20);
+            }
+            int foodLevel = this.namedTag.getInt("foodLevel");
+            if (!this.namedTag.contains("FoodSaturationLevel")) {
+                this.namedTag.putFloat("FoodSaturationLevel", 20);
+            }
+            float foodSaturationLevel = this.namedTag.getFloat("foodSaturationLevel");
+            this.foodData = new PlayerFood(this, foodLevel, foodSaturationLevel);
+
+            this.server.addOnlinePlayer(this, false);
+
+            PlayerLoginEvent ev;
+            this.server.getPluginManager().callEvent(ev = new PlayerLoginEvent(this, "Plugin reason"));
+            if (ev.isCancelled()) {
+                this.close(this.getLeaveMessage(), ev.getKickMessage());
+
+                return;
+            }
+
+            this.loggedIn = true;
+
+            if (this.isCreative()) {
+                this.inventory.setHeldItemSlot(0);
+            } else {
+                this.inventory.setHeldItemSlot(this.inventory.getHotbarSlotIndex(0));
+            }
+
+            if (this.isSpectator()) this.keepMovement = true;
+
+            PlayStatusPacket statusPacket = new PlayStatusPacket();
+            statusPacket.status = PlayStatusPacket.LOGIN_SUCCESS;
+            this.dataPacket(statusPacket);
+
+            if (this.spawnPosition == null && this.namedTag.contains("SpawnLevel") && (level = this.server.getLevelByName(this.namedTag.getString("SpawnLevel"))) != null) {
+                this.spawnPosition = new Position(this.namedTag.getInt("SpawnX"), this.namedTag.getInt("SpawnY"), this.namedTag.getInt("SpawnZ"), level);
+            }
+
+            Position spawnPosition = this.getSpawn();
+
+            if (this.isFirstTimeLogin) {
+                StartGamePacket startGamePacket = new StartGamePacket();
+                startGamePacket.seed = -1;
+                startGamePacket.dimension = (byte) (getLevel().getDimension() & 0xFF);
+                startGamePacket.x = (float) this.x;
+                startGamePacket.y = (float) this.y;
+                startGamePacket.z = (float) this.z;
+                startGamePacket.spawnX = (int) spawnPosition.x;
+                startGamePacket.spawnY = (int) spawnPosition.y;
+                startGamePacket.spawnZ = (int) spawnPosition.z;
+                startGamePacket.generator = 1; //0 old, 1 infinite, 2 flat
+                startGamePacket.gamemode = this.gamemode & 0x01;
+                startGamePacket.eid = 0; //Always use EntityID as zero for the actual player
+                startGamePacket.b1 = true;
+                startGamePacket.b2 = true;
+                startGamePacket.b3 = false;
+                startGamePacket.unknownstr = "";
+                this.dataPacket(startGamePacket);
+            }
+
+            SetTimePacket setTimePacket = new SetTimePacket();
+            setTimePacket.time = this.level.getTime();
+            setTimePacket.started = !this.level.stopTime;
+            this.dataPacket(setTimePacket);
+
+            SetSpawnPositionPacket setSpawnPositionPacket = new SetSpawnPositionPacket();
+            setSpawnPositionPacket.x = (int) spawnPosition.x;
+            setSpawnPositionPacket.y = (int) spawnPosition.y;
+            setSpawnPositionPacket.z = (int) spawnPosition.z;
+            this.dataPacket(setSpawnPositionPacket);
+        }
+
+        if (this.isFirstTimeLogin) {
+            if (this.needSlowLogin == 0) {
+                return;
+            } else {
+                if (System.currentTimeMillis() - this.needSlowLogin < 100) {
                     return;
                 }
             }
         }
-
-        this.setNameTag(this.username);
-
-        CompoundTag nbt = this.server.getOfflinePlayerData(this.username);
-        if (nbt == null) {
-            this.close(this.getLeaveMessage(), "Invalid data");
-
-            return;
-        }
-
-        this.playedBefore = (nbt.getLong("lastPlayed") - nbt.getLong("firstPlayed")) > 1;
-
-        nbt.putString("NameTag", this.username);
-
-        int exp = nbt.getInt("EXP");
-        int expLevel = nbt.getInt("expLevel");
-        this.setExperience(exp, expLevel);
-
-        this.gamemode = nbt.getInt("playerGameType") & 0x03;
-        if (this.server.getForceGamemode()) {
-            this.gamemode = this.server.getGamemode();
-            nbt.putInt("playerGameType", this.gamemode);
-        }
-
-        this.allowFlight = this.isCreative();
-
-        Level level;
-        if ((level = this.server.getLevelByName(nbt.getString("Level"))) == null) {
-            this.setLevel(this.server.getDefaultLevel());
-            nbt.putString("Level", this.level.getName());
-            nbt.getList("Pos", DoubleTag.class)
-                    .add(new DoubleTag("0", this.level.getSpawnLocation().x))
-                    .add(new DoubleTag("1", this.level.getSpawnLocation().y))
-                    .add(new DoubleTag("2", this.level.getSpawnLocation().z));
-        } else {
-            this.setLevel(level);
-        }
-
-        //todo achievement
-        nbt.putLong("lastPlayed", System.currentTimeMillis() / 1000);
-
-        if (this.server.getAutoSave()) {
-            this.server.saveOfflinePlayerData(this.username, nbt, true);
-        }
-
-        ListTag<DoubleTag> posList = nbt.getList("Pos", DoubleTag.class);
-
-        super.init(this.level.getChunk((int) posList.get(0).data >> 4, (int) posList.get(2).data >> 4, true), nbt);
-
-        if (!this.namedTag.contains("foodLevel")) {
-            this.namedTag.putInt("foodLevel", 20);
-        }
-        int foodLevel = this.namedTag.getInt("foodLevel");
-        if (!this.namedTag.contains("FoodSaturationLevel")) {
-            this.namedTag.putFloat("FoodSaturationLevel", 20);
-        }
-        float foodSaturationLevel = this.namedTag.getFloat("foodSaturationLevel");
-        this.foodData = new PlayerFood(this, foodLevel, foodSaturationLevel);
-
-        this.server.addOnlinePlayer(this, false);
-
-        PlayerLoginEvent ev;
-        this.server.getPluginManager().callEvent(ev = new PlayerLoginEvent(this, "Plugin reason"));
-        if (ev.isCancelled()) {
-            this.close(this.getLeaveMessage(), ev.getKickMessage());
-
-            return;
-        }
-
-        this.loggedIn = true;
-
-        if (this.isCreative()) {
-            this.inventory.setHeldItemSlot(0);
-        } else {
-            this.inventory.setHeldItemSlot(this.inventory.getHotbarSlotIndex(0));
-        }
-
-        if (this.isSpectator()) this.keepMovement = true;
-
-        PlayStatusPacket statusPacket = new PlayStatusPacket();
-        statusPacket.status = PlayStatusPacket.LOGIN_SUCCESS;
-        this.dataPacket(statusPacket);
-
-        if (this.spawnPosition == null && this.namedTag.contains("SpawnLevel") && (level = this.server.getLevelByName(this.namedTag.getString("SpawnLevel"))) != null) {
-            this.spawnPosition = new Position(this.namedTag.getInt("SpawnX"), this.namedTag.getInt("SpawnY"), this.namedTag.getInt("SpawnZ"), level);
-        }
-
-        Position spawnPosition = this.getSpawn();
-
-        if (this.isFirstTimeLogin) {
-            StartGamePacket startGamePacket = new StartGamePacket();
-            startGamePacket.seed = -1;
-            startGamePacket.dimension = (byte) (getLevel().getDimension() & 0xFF);
-            startGamePacket.x = (float) this.x;
-            startGamePacket.y = (float) this.y;
-            startGamePacket.z = (float) this.z;
-            startGamePacket.spawnX = (int) spawnPosition.x;
-            startGamePacket.spawnY = (int) spawnPosition.y;
-            startGamePacket.spawnZ = (int) spawnPosition.z;
-            startGamePacket.generator = 1; //0 old, 1 infinite, 2 flat
-            startGamePacket.gamemode = this.gamemode & 0x01;
-            startGamePacket.eid = 0; //Always use EntityID as zero for the actual player
-            startGamePacket.b1 = true;
-            startGamePacket.b2 = true;
-            startGamePacket.b3 = false;
-            startGamePacket.unknownstr = "";
-            this.dataPacket(startGamePacket);
-        }
-
-        SetTimePacket setTimePacket = new SetTimePacket();
-        setTimePacket.time = this.level.getTime();
-        setTimePacket.started = !this.level.stopTime;
-        this.dataPacket(setTimePacket);
-
-        SetSpawnPositionPacket setSpawnPositionPacket = new SetSpawnPositionPacket();
-        setSpawnPositionPacket.x = (int) spawnPosition.x;
-        setSpawnPositionPacket.y = (int) spawnPosition.y;
-        setSpawnPositionPacket.z = (int) spawnPosition.z;
-        this.dataPacket(setSpawnPositionPacket);
 
         if (this.isFirstTimeLogin) {
             SetHealthPacket pk = new SetHealthPacket();
@@ -262,9 +276,9 @@ public class SynapsePlayer extends Player {
         }
 
         this.forceMovement = this.teleportPosition = this.getPosition();
+        this.needSlowLogin = 0;
 
         this.server.onPlayerLogin(this);
-
 
     }
 
@@ -298,6 +312,7 @@ public class SynapsePlayer extends Player {
         if (!this.isSynapseLogin) {
             return super.onUpdate(currentTick);
         }
+        if (this.isFirstTimeLogin && this.needSlowLogin != 0) this.processLogin();
         if((System.currentTimeMillis() - this.lastPacketTime) >= 5 * 60 * 1000){//5 minutes time out
             this.close("", "timeout");
             return false;
