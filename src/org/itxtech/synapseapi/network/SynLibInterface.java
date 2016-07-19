0eff2cee37b5bcc45a863f3bf0dc70aa2a4df20b
boybook <fangyizhou1997@qq.com>
2016-07-19 10:36:53 +0800

    Please update the Nukkit! It is a new interface!

--- src/org/itxtech/synapseapi/network/SynLibInterface.java
+++ src/org/itxtech/synapseapi/network/SynLibInterface.java
@@ -18,6 +18,11 @@ public class SynLibInterface implements SourceInterface {
     }
 
     @Override
+    public int getNetworkLatency(Player player) {
+        return 0;
+    }
+
+    @Override
     public void emergencyShutdown() {
     }
 
